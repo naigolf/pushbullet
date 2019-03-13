3b7aa01b47465e5cbd7350e5fa500ce11383c4ef
nai golf <niy_golf@hotmail.com>
2019-03-13 12:21:26 +0700

    Create websocket.js

---
+++ websocket.js
@@ -0,0 +1,23 @@
+var websocket;
+var apiKey = document.getElementById('api-key');
+var submit = document.getElementById('submit');
+var messages = document.getElementById('messages');
+submit.onclick = function() {
+    if (websocket != null) {
+        websocket.close();
+    }
+    
+    websocket = new WebSocket('wss://stream.pushbullet.com/websocket/' + apiKey.value);
+    websocket.onopen = function(e) {
+        messages.innerHTML += "<p>WebSocket onopen</p>";
+    }
+    websocket.onmessage = function(e) {
+        messages.innerHTML += "<p>ok</p>"+"<p>" + e.data + "</p>";
+    }
+    websocket.onerror = function(e) {
+        messages.innerHTML += "<p>WebSocket onerror</p>";
+    }
+    websocket.onclose = function(e) {
+        messages.innerHTML += "<p>WebSocket onclose</p>";
+    }
+}
